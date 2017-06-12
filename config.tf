a9c0002173a9c79cd0bdef8ed27c0f5eabffd44c
Lance Kuo <lancekuo@gmail.com>
2017-06-12 16:57:20 -0400

    fix

--- config.tf
+++ config.tf
@@ -14,10 +14,7 @@ resource "null_resource" "ssh_trigger" {
     }
 
     provisioner "local-exec" {
-        command = "cat <<EOF > test.sh
-${data.template_file.script.rendered}
-EOF
-"
+        command = "cat ${data.template_file.script.rendered} > test.sh"
     }
 }
 
